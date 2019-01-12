c3412576a4ae18a6b7ba18a54782064d626d9925
tilan <tilzw27@gmail.com>
2019-01-12 20:31:00 +0530

    adding the custom list

--- app/src/main/java/com/example/tilan/photoviewtest/nodeStateActivity.java
+++ app/src/main/java/com/example/tilan/photoviewtest/nodeStateActivity.java
@@ -1,14 +1,31 @@
 package com.example.tilan.photoviewtest;
 
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import static com.example.tilan.photoviewtest.MainActivity.IP;
+
 public class nodeStateActivity extends AppCompatActivity {
 
-    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
-            "WebOS","Ubuntu","Windows7","Max OS X"};
+    String[] mobileArray = {"Node_1","Node_2","Node_3","Node_4",
+            "Node_5","Node_6","Node_7","Node_8"};
+    int NODE_STATE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +35,55 @@ public class nodeStateActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.nodes);
         listView.setAdapter(adapter);
+    }
+
+    public class GetData extends AsyncTask<Void, Void, JSONArray> {
+
+        @Override
+        protected JSONArray doInBackground(Void... voids) {
+            String str = "http://"+IP+"/select.php";
+            URLConnection urlConn = null;
+            BufferedReader bufferedReader = null;
+            try {
+                URL url = new URL(str);
+                urlConn = url.openConnection();
+                bufferedReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+
+                StringBuffer stringBuffer = new StringBuffer();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuffer.append(line);
+                }
+
+                return new JSONArray(stringBuffer.toString());
+            } catch (Exception ex) {
+                Log.e("App", "yourDataTask", ex);
+                return null;
+            } finally {
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray response) {
+            if (response != null) {
+                try {
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject jsonobject = response.getJSONObject(i);
+                        NODE_STATE=jsonobject.getInt("NODE_STATE");
+                        Log.d("State",String.valueOf(NODE_STATE));
+
+                    }
+                } catch (JSONException ex) {
+                    Log.e("App", "Failure", ex);
+                }
+            }
+        }
     }
 }
